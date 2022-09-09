77e0975f2d50e6373114a8b75c451bd93a04a558
Kosisochukwu <realitynuel@gmail.com>
2022-09-08 17:05:57 -0700

    Sum of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+	long longType;
+
+	printf("size of int: %d bytes\n", sizeof(inType));
+	printf("size of float: %d bytes\n", sizeof(float));
+	printf("size of double: %d bytes\n", sizeof(char));
+	printf("size of long: %db bytes\n", sizeof(long));
+	return (0);
+}
+
